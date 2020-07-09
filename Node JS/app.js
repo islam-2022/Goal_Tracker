9bf413ec2eb15a7d0e6668cda33ee684c1b7b8a6
Islam Yousry <islamyousry16@gmail.com>
2020-07-10 01:29:32 +0200

    test server

--- Node JS/app.js
+++ Node JS/app.js
@@ -3,7 +3,9 @@ var app = express();
 
 
 
-
+app.get('/',function(req,res){
+	res.send('Hello World!');
+});
 
 // listen to environment varibale port or 3000 otherwise.
 var port = process.env.PORT || 3000;
