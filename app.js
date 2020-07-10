fc0fdb86b90fea39558acab9a65fdc00e6ba3e6a
Islam Yousry <islamyousry16@gmail.com>
2020-07-10 13:14:39 +0200

    test server

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/',function(req,res){
 	res.send('Hello World!');
 });
 
+app.get('/test',function(req,res){
+	res.send('you are in testing page!');
+});
+
 // listen to environment varibale port or 3000 otherwise.
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
