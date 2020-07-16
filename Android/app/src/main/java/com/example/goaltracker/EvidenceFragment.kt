5f3ada56435902ed4c499777cbe88d8464e3cc45
klanti islam <klantyislam@yahoo.com>
2020-07-16 17:53:18 -0400

    updated fragment name from EviFrag to EvidenceFragment and added a dimensions file

--- Android/app/src/main/java/com/example/goaltracker/EvidenceFragment.kt
+++ Android/app/src/main/java/com/example/goaltracker/EvidenceFragment.kt
@@ -1,5 +1,5 @@
 import androidx.fragment.app.Fragment
 import com.example.goaltracker.R
 
-class EviFrag:Fragment(R.layout.fragment_evidence) {
+class EvidenceFragment:Fragment(R.layout.fragment_evidence) {
 }
